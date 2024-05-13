c0c65a71d55ce001ad3dc03a4063285cb3162b70
Christo de Wet <Christo.dewet@outlook.com>
2024-05-13 12:22:11 +0200

    Add project files.

---
+++ DapperDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using DapperDemo.Repository;
+
+namespace DapperDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ICompanyRepository _companyRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public EmployeesController(ICompanyRepository companyRepository, IEmployeeRepository employeeRepository)
+        {
+            _companyRepository = companyRepository;
+            _employeeRepository = employeeRepository;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(_employeeRepository.GetAll());
+        }
+
+        //// GET: Employees/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var company = _companyRepository.Find(id.GetValueOrDefault());
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(company);
+        //}
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> companyList = _companyRepository.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.CompanyId.ToString()
+            });
+
+            ViewBag.CompanyList = companyList;
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public async Task<IActionResult> CreatePOST()
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Add(Employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employee = _employeeRepository.Find(id.GetValueOrDefault());
+
+            IEnumerable<SelectListItem> companyList = _companyRepository.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.CompanyId.ToString()
+            });
+
+            ViewBag.CompanyList = companyList;
+
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            return View(Employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id != Employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Update(Employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            _employeeRepository.Remove(id.GetValueOrDefault());
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ DapperDemo/Repository/CompanyRepositoryDapper.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryDapper : ICompanyRepository
+    {
+        private IDbConnection db;
+
+        public CompanyRepositoryDapper(IConfiguration configuration)
+        {
+            this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public Company Add(Company company)
+        {
+            var sql = "INSERT INTO Companies (Name, Address, City, State, PostalCode) VALUES (@Name, @Address, @City, @State, @PostalCode);" + "SELECT CAST(SCOPE_IDENTITY() as int);";
+            var id = db.Query<int>(sql, new
+            {
+                @Name = company.Name,
+                @Address = company.Address,
+                @City = company.City,
+                @State = company.State,
+                @PostalCode = company.PostalCode
+            }).Single();
+            company.CompanyId = id;
+            return company;
+        }
+
+        public Company Find(int id)
+        {
+            var sql = "SELECT * FROM Companies WHERE CompanyId = @CompanyId";
+            return db.Query<Company>(sql, new 
+            { 
+                @CompanyId = id
+            }).Single();
+        }
+
+        public List<Company> GetAll()
+        {
+            var sql = "SELECT * FROM Companies";
+            return db.Query<Company>(sql).ToList();
+        }
+
+        public void Remove(int id)
+        {
+            var sql = "DELETE FROM Companies WHERE CompanyId = @CompanyId;";
+            db.Execute(sql, new 
+            { 
+                @CompanyId = id
+            });
+            // TODO: Confirm that the company need to be removed
+        }
+
+        public Company Update(Company company)
+        {
+            var sql = "UPDATE Companies SET Name = @Name, Address = @Address, City = @City, State = @State, PostalCode = @PostalCode WHERE CompanyId = @CompanyId;";
+            db.Execute(sql, new
+            {
+                @Name = company.Name,
+                @Address = company.Address,
+                @City = company.City,
+                @State = company.State,
+                @PostalCode = company.PostalCode,
+                @CompanyId = company.CompanyId
+            });
+            return company;
+        }
+    }
+}
